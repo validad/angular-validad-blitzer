d51d9a23ccb8c6ddb48a5a0f039cf922fcf5f9ac
Johannes Troeger <johannes.troeger@gmail.com>
2014-07-26 19:07:35 +0200

    Release v0.3.0

--- angular-validad-blitzer.js
+++ angular-validad-blitzer.js
@@ -1,42 +1,52 @@
 ;
 (function () {
-  angular.module('angular-validad-blitzer.directive', []).directive('blitzer', function () {
-    'use strict';
-    return {
-      restrict: 'E',
-      replace: true,
-      controller: [
-        '$rootScope',
-        '$scope',
-        '$timeout',
-        'blitzer',
-        function ($rootScope, $scope, $timeout, blitzer) {
-          $scope.blitzers = [];
-          blitzer.subscribe(function (state, blitzers) {
-            $scope.blitzers.push({
-              state: state,
-              msg: blitzers
+  angular.module('angular-validad-blitzer.directive', []).directive('blitzer', [
+    'blitzer',
+    function (blitzer) {
+      'use strict';
+      return {
+        restrict: 'E',
+        replace: true,
+        scope: { group: '@' },
+        template: blitzer.template || function () {
+          return [
+            '<div class="m-blitzer m-blitzer--small" ng-show="!!blitzers.length">',
+            '   <span',
+            '       class="m-blitzer__msg is-{{blitzer.state}}"',
+            '       ng-click="close($index)"',
+            '       ng-repeat="blitzer in blitzers"',
+            '   >',
+            '       <span>{{blitzer.message}}</span>',
+            '   </span>',
+            '</div>'
+          ].join('');
+        },
+        controller: [
+          '$rootScope',
+          '$scope',
+          '$timeout',
+          'blitzer',
+          function ($rootScope, $scope, $timeout, blitzer) {
+            $scope.blitzers = [];
+            blitzer.subscribe(function (state, group, incomingBlitzers) {
+              if (group === $scope.group) {
+                angular.forEach(incomingBlitzers, function (blitzer) {
+                  this.push({
+                    state: state,
+                    message: blitzer
+                  });
+                }, $scope.blitzers);
+                return;
+              }
             });
-          });
-          $scope.close = function (index) {
-            $scope.blitzers.splice(index, 1);
-          };
-        }
-      ],
-      template: function () {
-        return [
-          '<div',
-          '   class="alert alert-{{blitzer.state}}"',
-          '   role="alert"',
-          '   ng-click="close($index)"',
-          '   ng-repeat="blitzer in blitzers"',
-          '>',
-          '   {{blitzer.msg}}',
-          '</div>'
-        ].join('');
-      }
-    };
-  });
+            $scope.close = function (index) {
+              $scope.blitzers.splice(index, 1);
+            };
+          }
+        ]
+      };
+    }
+  ]);
   angular.module('angular-validad-blitzer', [
     'angular-validad-blitzer.provider',
     'angular-validad-blitzer.directive'
@@ -44,20 +54,25 @@
   angular.module('angular-validad-blitzer.provider', []).provider('blitzer', function () {
     'use strict';
     var states = ['notify'];
+    var template = null;
     this.defineStates = function (customStates) {
       states = states.concat(customStates);
     };
-    this.$get = function () {
-      return Blitzer.build(states);
+    this.defineTemplate = function (tmplStr) {
+      template = tmplStr;
     };
-    var Blitzer = function () {
+    this.$get = function () {
+      return Blitzer.build(states, template);
+    };
+    var Blitzer = function (template) {
       this.listeners = [];
       this.subjects = {};
+      this.template = template;
     };
     Blitzer.prototype.add = function (name) {
-      this.subjects[name] = function () {
-        var args = Array.prototype.slice.call(arguments);
-        this.fire(name, args);
+      this.subjects[name] = function (group) {
+        var args = Array.prototype.slice.call(arguments, 1);
+        this.fire(name, group, args);
       }.bind(this);
       // extend Blitzer with subject as function
       this[name] = this.subjects[name];
@@ -71,8 +86,8 @@
         listener.apply(this, args);
       }.bind(this));
     };
-    Blitzer.build = function (states) {
-      var blitzer = new Blitzer();
+    Blitzer.build = function (states, template) {
+      var blitzer = new Blitzer(template);
       angular.forEach(states, function (state) {
         blitzer.add(state);
       });
