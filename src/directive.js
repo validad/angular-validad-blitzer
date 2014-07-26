13f0ddc4426a21ca6223ec62995a4c2a87383f98
Johannes Troeger <johannes.troeger@gmail.com>
2014-07-26 18:20:59 +0200

    feat(directive): add custom template

--- src/directive.js
+++ src/directive.js
@@ -1,38 +1,38 @@
 angular.module('angular-validad-blitzer.directive', [])
 
-.directive('blitzer', function () {
+.directive('blitzer', function (blitzer) {
 
     'use strict';
 
     return {
         restrict: 'E',
         replace: true,
+        template: blitzer.template || function () {
+            return ['<div',
+                    '   class="alert alert-{{blitzer.state}}"',
+                    '   role="alert"',
+                    '   ng-click="close($index)"',
+                    '   ng-repeat="blitzer in blitzers"',
+                    '>',
+                    '   {{blitzer.msg}}',
+                    '</div>'
+                ].join('');
+        },
         controller: function ($rootScope, $scope, $timeout, blitzer) {
             $scope.blitzers = [];
 
-            blitzer.subscribe(function (state, blitzers) {
-                $scope.blitzers.push({
-                    state: state,
-                    msg: blitzers
-                });
+            blitzer.subscribe(function (state, incomingBlitzers) {
+                angular.forEach(incomingBlitzers, function (blitzer) {
+                    this.push({
+                        state: state,
+                        msg: blitzer
+                    });
+                }, $scope.blitzers);
             });
 
             $scope.close = function (index) {
                 $scope.blitzers.splice(index, 1);
             };
-
-        },
-        template: function () {
-            return [
-                '<div',
-                '   class="alert alert-{{blitzer.state}}"',
-                '   role="alert"',
-                '   ng-click="close($index)"',
-                '   ng-repeat="blitzer in blitzers"',
-                '>',
-                '   {{blitzer.msg}}',
-                '</div>'
-            ].join('');
         }
     };
 });
--- src/provider.js
+++ src/provider.js
@@ -5,18 +5,22 @@ angular.module('angular-validad-blitzer.provider', [])
     'use strict';
 
     var states = ['notify'];
+    var template = null;
 
     this.defineStates = function (customStates) {
         states = states.concat(customStates);
     };
-
+    this.defineTemplate = function (tmplStr) {
+        template = tmplStr;
+    };
     this.$get = function () {
-        return Blitzer.build(states);
+        return Blitzer.build(states, template);
     };
 
-    var Blitzer = function () {
+    var Blitzer = function (template) {
         this.listeners = [];
         this.subjects = {};
+        this.template = template;
     };
 
     Blitzer.prototype.add = function (name) {
@@ -39,8 +43,8 @@ angular.module('angular-validad-blitzer.provider', [])
         }.bind(this));
     };
 
-    Blitzer.build = function (states) {
-        var blitzer = new Blitzer();
+    Blitzer.build = function (states, template) {
+        var blitzer = new Blitzer(template);
         angular.forEach(states, function (state) {
             blitzer.add(state);
         });
